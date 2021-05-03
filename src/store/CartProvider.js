704fc0fc6a033792e1dd3ae66362691c3f299428
Smit Vekaria <smit.vce19@sot.pdpu.ac.in>
2021-05-04 02:01:37 +0530

    Making the items removable

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -25,11 +25,32 @@ const cartReducer = (state, action) => {
         } else {
             updatedItems = state.items.concat(action.item);
         }
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
 
-        const updatedTotalAmount =
-            state.totalAmount + action.item.price * action.item.amount;
         return { items: updatedItems, totalAmount: updatedTotalAmount };
     }
+
+    if(action.type === 'REMOVE') {
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.id
+        );
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        let updatedItems;
+
+        if(existingCartItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        } else {
+            const updatedItem = {...existingCartItem, amount: existingCartItem.amount - 1};
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        
+        const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+        return { items: updatedItems, totalAmount: updatedTotalAmount };
+    }
+
     return defaultCartState;
 };
 
