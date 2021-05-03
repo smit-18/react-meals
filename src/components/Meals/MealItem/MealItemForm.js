e2d02f611e92fa7e0b52581e35214f45bc878f7e
Smit Vekaria <smit.vce19@sot.pdpu.ac.in>
2021-05-03 18:34:57 +0530

    Bug fixes

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -4,14 +4,17 @@ import classes from './MealItemForm.module.css';
 const MealItemForm = (props) => {
     return (
         <form className={classes.form}>
-            <Input label="Amount" input={{
-                id: 'amout',
-                type: 'number',
-                min: '1',
-                max: '5',
-                step: '1',
-                defaultValue: '1'
-            }} />
+            <Input
+                label="Amount"
+                input={{
+                    id: 'amount_' + props.id,
+                    type: 'number',
+                    min: '1',
+                    max: '5',
+                    step: '1',
+                    defaultValue: '1',
+                }}
+            />
             <button>+ Add</button>
         </form>
     );
